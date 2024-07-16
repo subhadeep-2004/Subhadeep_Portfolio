60d71ffbae2fae1e16b970584f096e97e2f5cb64
Subhadeep Bhadra <138879517+subhadeep-2004@users.noreply.github.com>
2024-07-17 00:49:27 +0530

    Add files via upload

---
+++ assets/js/script.js
@@ -0,0 +1,95 @@
+let header = document.querySelector("header");
+let menu = document.querySelector("#menu-icon");
+let navbar = document.querySelector(".navbar");
+let navLinks = document.querySelectorAll(".navbar a");
+
+window.addEventListener("scroll", () => {
+  header.classList.toggle("shadow", window.scrollY > 0);
+});
+// const menuIcon = document.getElementById('menu-icon');
+menu.onclick = () => {
+  navbar.classList.toggle("active");
+  // menuIcon.classList.toggle('bx-menu');
+
+  
+};
+window.onscroll = () => {
+  navbar.classList.remove("active");
+  menu.classList.remove('bx-x');
+  menu.classList.add('bx-menu');
+};
+
+document.getElementById('menu-icon').addEventListener('click', function() {
+  this.classList.toggle('bx-menu');
+  this.classList.toggle('bx-x');
+});
+
+
+// navLinks.forEach(link => {
+//   link.addEventListener('click', () => {
+//     navbar.classList.remove("active");
+//     menu.classList.remove('bx-x');
+//     menu.classList.add('bx-menu');
+//   });
+// });
+// Dark Mode / light mode
+let darkmode = document.querySelector("#darkmode");
+
+darkmode.onclick = () => {
+  if (darkmode.classList.contains("bx-moon")) {
+    darkmode.classList.replace("bx-moon", "bx-sun");
+    document.body.classList.add("active");
+  } else {
+    darkmode.classList.replace("bx-sun", "bx-moon");
+    document.body.classList.remove("active");
+  }
+};
+const tabs = document.querySelectorAll("[data-target]"),
+  tabContents = document.querySelectorAll("[data-content]");
+
+tabs.forEach((tab) => {
+  tab.addEventListener("click", () => {
+    const target = document.querySelector(tab.dataset.target);
+
+    tabContents.forEach((tabContent) => {
+      tabContent.classList.remove("qualification__active");
+    });
+    target.classList.add("qualification__active");
+
+    tabs.forEach((tab) => {
+      tab.classList.remove("qualification__active");
+    });
+    tab.classList.add("qualification__active");
+  });
+});
+
+
+
+
+
+// motion
+document.addEventListener('DOMContentLoaded', function () {
+  const sections = document.querySelectorAll('.section');
+
+  const observerOptions = {
+      root: null,
+      rootMargin: '0px',
+      threshold: 0.1
+  };
+
+  const observerCallback = (entries, observer) => {
+      entries.forEach(entry => {
+          if (entry.isIntersecting) {
+              entry.target.classList.add('show');
+          } else {
+              entry.target.classList.remove('show');
+          }
+      });
+  };
+
+  const observer = new IntersectionObserver(observerCallback, observerOptions);
+
+  sections.forEach(section => {
+      observer.observe(section);
+  });
+});
